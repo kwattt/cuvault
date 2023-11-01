92ba619e0cd5b13b8508e3992e00533f472be5c0
Hirane <diego.preciado@alumno.udg.mx>
2023-10-31 18:40:36 -0600

    Ayuda Formatting

--- frontend/src/views/Ayuda/index.tsx
+++ frontend/src/views/Ayuda/index.tsx
@@ -1,44 +1,89 @@
-
+import {Box, Text} from "@chakra-ui/react";
 
 const Ayuda = () => {
-  return <b>
+  return <>
+  <Box
+    textAlign='center'
+    p='2em'
+  >
+  Frequently Asked Questions (FAQ)
+  </Box>
+  <Box
+    textAlign='left'
+    p='2em'
+  >
+    <b>¿Qué es CuVault?</b>
+    <Text>
+    CuVault es una aplicación especializada en temas relacionados a la Ingeniería en Computación, la cual ofrece una forma sencilla de acceder a información relevante con recomendaciones personalizadas usando nuestro modelo de inteligencia artificial que utiliza el algoritmo de similaridad de cosenos para ofrecer recomendaciones pertinentes.
+    
+    </Text><br/>
+    <b>¿Quién puede utilizar CuVault?</b>
+    <Text>
+    Cualquier persona con interés en temas relacionados a la Ingeniería en Computación
+    </Text><br/>
+    <b>¿Cómo puedo utilizar CuVault?</b>
+    <Text>
+    Utilizar CuVault es tan sencillo como acceder a la sección de “Inicio” e introducir un concepto en la barra de búsqueda, una vez hecha la búsqueda nuestro modelo te mostrará la definición del concepto y un listado de recomendaciones así como fuentes de consulta en las cuales el usuario podrá encontrar más información así como las categorías relacionadas.
+    </Text><br/>
+    <b>¿Tiene algún costo la aplicación?</b>
+    <Text>
+    CuVault es completamente gratis para cualquier persona.
+    </Text><br/>
+    <b>¿Necesito crear una cuenta para utilizar la aplicación?</b>
+    <Text>
+    No es necesario crear una cuenta para utilizar la aplicación, en un futuro el usuario podrá crear una cuenta para acceder a la sección de “Conversaciones”, dónde podrá acceder a foros con otros usuarios registrados en la aplicación (funcionalidad en desarrollo).
+    </Text><br/>
+    <b>¿La aplicación se trata de un proyecto open source?</b>
+    <Text>
+    La aplicación está desarrollada bajo la licencia MIT, la cual es una licencia de software libre permisiva.
+    </Text><br/>
+    <b>¿Dónde puedo consultar el código fuente del proyecto?</b>
+    <Text>
+    El código fuente del proyecto puede ser consultado en el siguiente repositorio.
+    </Text><br/>
+  </Box>
 
-Frequently Asked Questions (FAQ)
+<Box
+      textAlign='center'
+      p='2em'
+>
+  <b>DISCLAIMER:</b>
+</Box>
 
-¿Qué es CuVault?
-CuVault es una aplicación especializada en temas relacionados a la Ingeniería en Computación, la cual ofrece una forma sencilla de acceder a información relevante con recomendaciones personalizadas usando nuestro modelo de inteligencia artificial que utiliza el algoritmo de similaridad de cosenos para ofrecer recomendaciones pertinentes.
+<Box
+    textAlign='left'
+    p='2em'
 
-¿Quién puede utilizar CuVault?
-Cualquier persona con interés en temas relacionados a la Ingeniería en Computación
-
-¿Cómo puedo utilizar CuVault?
-Utilizar CuVault es tan sencillo como acceder a la sección de “Inicio” e introducir un concepto en la barra de búsqueda, una vez hecha la búsqueda nuestro modelo te mostrará la definición del concepto y un listado de recomendaciones así como fuentes de consulta en las cuales el usuario podrá encontrar más información así como las categorías relacionadas.
-
-¿Tiene algún costo la aplicación?
-CuVault es completamente gratis para cualquier persona.
-
-¿Necesito crear una cuenta para utilizar la aplicación?
-No es necesario crear una cuenta para utilizar la aplicación, en un futuro el usuario podrá crear una cuenta para acceder a la sección de “Conversaciones”, dónde podrá acceder a foros con otros usuarios registrados en la aplicación (funcionalidad en desarrollo).
-
-¿La aplicación se trata de un proyecto open source?
-La aplicación está desarrollada bajo la licencia MIT, la cual es una licencia de software libre permisiva.
-
-¿Dónde puedo consultar el código fuente del proyecto?
-El código fuente del proyecto puede ser consultado en el siguiente repositorio.
-DISCLAIMER:
-
+>
+<Text>
 La información proporcionada en esta aplicación web es de carácter general y se ofrece con fines informativos únicamente.
-
+</Text><br/>
+<Text>
 Hemos hecho todo lo posible para garantizar que la información contenida en esta aplicación web sea precisa y confiable. Sin embargo, la información proviene de diversas fuentes, y no nos hacemos responsables de ningún error u omisión. No garantizamos la exactitud, integridad ni utilidad de la información proporcionada, y no nos hacemos responsables de ningún daño o pérdida que pueda surgir de su uso.
-
+</Text><br/>
+<Text>
 Fuentes de consulta bibliográfica pueden proporcionarse para mayor comodidad, pero no nos hacemos responsables del contenido de dichas fuentes, ni respaldamos los productos o servicios que puedan ofrecer. La inclusión de fuentes bibliográficas de terceros no implica nuestra aprobación de la fuente o su contenido.
-
+</Text><br/>
+<Text>
 Al utilizar esta aplicación web, aceptas este descargo de responsabilidad y entiendes que la información proporcionada no debe ser utilizada como única fuente de información. Es responsabilidad del usuario verificar la exactitud de la información antes de usarla para cualquier propósito.
-
+</Text><br/>
+<Text>
 En ningún caso, los autores, desarrolladores, o cualquier otra persona involucrada en la creación, producción o distribución del software serán responsables de cualquier reclamación, daño o cualquier otra responsabilidad, ya sea en una acción legal, negligencia u otra, que surja de, o en conexión con, el software o el uso u otras interacciones con el software. Esto incluye, pero no se limita a, responsabilidad por daños indirectos, incidentales, especiales, consecuentes o punitivos.
+</Text><br/>
+ 
+
+
+
+
+</Box>
+
+
+
+
+
 
   
-  </b>
+  </>
 }
 
 export default Ayuda
